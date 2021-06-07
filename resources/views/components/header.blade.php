9704c567289ee568d5f46776fa992b867a75b923
sgtYuri <pvt.partner21@gmail.com>
2021-06-07 13:59:04 +0800

    card-body

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-redr">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,10 +7,12 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a href="/supplier" class=" text-white nav-link  @if($active=='supplier') active @endif">
+            <a href="/supplier" class=" text-black nav-link  @if($active=='supplier') active @endif">
+            Supplier
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Storgage</a>
+        </li> <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Storage</a>
+        </li>
           </li> <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Assets</a>
           </li>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div style="height:88vh;" class="bg-primary d-flex flex-column flex-shrink-0 p-3 bg-dark">
+{{-- <div style="height:88vh;" class="bg-primary d-flex flex-column flex-shrink-0 p-3 bg-dark">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
     <svg class="bi me-2" width="40" height="40"><use xlink:href="#bootstrap"></use></svg>
     <span class="fs-4 text-white">Dashboard</span>
@@ -87,4 +87,4 @@
         <li><a class="dropdown-item" href="#">Sign out</a></li>
     </ul>
     </div>
-</div>
+</div> --}}
